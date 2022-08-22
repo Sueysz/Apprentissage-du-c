aa7e02b315a7b0029400212ae4166268885d0de4
sueys <sueys@live.fr>
2022-08-22 17:18:57 +0200

    mon hello world en c

---
+++ main.c
@@ -0,0 +1,12 @@
+/* librairie pour inclure du languages pré-fait */
+#include <stdio.h>
+#include <stdlib.h>
+/* fonction principal " int main " et " printf " pour demander à afficher un message à l'écran.
+Le code commence toujours par la fonction main.
+*/
+int main()
+{
+    printf("bonjour!\nAu revoir\n");//Introduction.
+    return 0;//Le programme renvoie le nombre 0 si tous c'est bien passé si il renvois une autre valeurs c'est qu'il y à une erreur.
+}
+
